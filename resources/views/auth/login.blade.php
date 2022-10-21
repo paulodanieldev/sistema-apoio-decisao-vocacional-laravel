d7e81eff57ba0ee708ccd8c63789b2962da01d98
paulodiasdev <paulo.dias@veggi.io>
2022-10-21 11:29:18 -0300

    refactor: change register and login page template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,82 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+@include('dashboard.alerts')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<main>
+    <div class="container">
+
+    <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+            <div class="d-flex justify-content-center py-4">
+                <a href="{{ route('site') }}" class="logo d-flex align-items-center w-auto">
+                <img src="{{ asset('media/dashboard/logo.png') }}" alt="">
+                <span class="d-none d-lg-block">NiceAdmin</span>
+                </a>
+            </div><!-- End Logo -->
+
+            <div class="card mb-3">
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <div class="pt-4 pb-2">
+                        @yield('alerts')
+                        <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                        <p class="text-center small">Enter your email & password to login</p>
+                    </div>
+                    <form method="POST" action="{{ route('login') }}" class="row g-3 needs-validation" novalidate>
                         @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        <div class="col-12">
+                            <label for="email" class="form-label">Email</label>
+                            <div class="input-group has-validation">
+                                <span class="input-group-text" id="inputGroupPrepend">@</span>
+                                <input type="email" name="email" class="form-control" id="email" required>
+                                <div class="invalid-feedback">Please enter your email.</div>
                             </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="col-12">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" name="password" class="form-control" id="password" required>
+                            <div class="invalid-feedback">Please enter your password!</div>
                         </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                        <div class="col-12">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember" value="true" id="remember">
+                                <label class="form-check-label" for="remember">Remember me</label>
                             </div>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                        <div class="col-12">
+                            <button class="btn btn-primary w-100" type="submit">Login</button>
                         </div>
+                        <div class="col-12">
+                            <p class="small mb-0">Don't have account? <a href="{{ route('register') }}">Create an account</a></p>
+                        </div>
+                        @if (Route::has('password.request'))
+                            <div class="col-12">
+                                <p class="small mb-0">Don't remember your password? <a href="{{ route('password.request') }}">Forgot Your Password?</a></p>
+                            </div>
+                        @endif
                     </form>
                 </div>
             </div>
+
+            <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+
+            </div>
         </div>
+        </div>
+
+    </section>
+
     </div>
-</div>
+</main><!-- End #main -->
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 @endsection
