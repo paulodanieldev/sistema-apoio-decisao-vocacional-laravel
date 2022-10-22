d06812229622ee9cf584913a5cfe1e1b1d69a726
paulodiasdev <paulo.dias@veggi.io>
2022-10-22 13:35:50 -0300

    feat: create quiz and scool grades models

---
+++ app/Models/QuizQuestions.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class QuizQuestions extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'quiz_questions';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'quiz_id',
+        'question',
+        'quiz_question_type_id',
+        'is_active'
+    ];
+
+    /**
+     * Instancing dates of the table
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+}
---
+++ app/Models/SchoolReports.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SchoolReports extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'school_reports';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'user_id',
+        'school_year',
+        'school_level_id',
+        'school_grade_id'
+    ];
+
+    /**
+     * Instancing dates of the table
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+}
